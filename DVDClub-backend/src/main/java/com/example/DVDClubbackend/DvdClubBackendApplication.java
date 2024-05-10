d911010567e21a6c5a9b6584baaadb7197b1e8d3
igorr00 <igor.miskic07@gmail.com>
2024-05-10 21:30:15 +0200

    init

---
+++ DVDClub-backend/src/main/java/com/example/DVDClubbackend/DvdClubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DVDClubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdClubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdClubBackendApplication.class, args);
+	}
+
+}
